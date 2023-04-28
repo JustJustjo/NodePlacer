d2c8fa3c8ef69fdeeb0847a5d06339616330393c
Justin Kilk <justin@kilk.com>
2023-04-27 17:07:58 -0700

    added menubar

---
+++ src/main/kotlin/com/kilk/MenuDeck.kt
@@ -0,0 +1,22 @@
+package com.kilk
+
+import javafx.scene.control.Menu
+import javafx.scene.control.MenuBar
+import javafx.scene.control.MenuItem
+
+object MenuDeck: MenuBar() {
+    val fileMenu = Menu("File")
+
+    val saveItem = MenuItem("Save")
+    val loadItem = MenuItem("Load")
+    init {
+        saveItem.setOnAction {
+//            Dashboard.save()
+        }
+        loadItem.setOnAction {
+//            Dashboard.load()
+        }
+        fileMenu.items.addAll(saveItem, loadItem)
+        menus.addAll(fileMenu)
+    }
+}
